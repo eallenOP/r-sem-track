3695f8e544e6d944295e119345c8e36467a2d130
Elise <elise.allen@op.ac.nz>
2018-03-23 16:42:42 +1300

    Complete semester creation

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,10 +1,21 @@
 class SemestersController < ApplicationController
+    def show
+        @semester = Semester.find(params[:id])
+    end
+    
     def new
 
     end
 
     def create
-        render plain: params[:semester].inspect
+        @semester = Semester.new(semester_params)
+
+        @semester.save
+        redirect_to @semester
     end
 
+    private
+    def semester_params
+        params.require(:semester).permit(:semester_num, :year, :start_date)
+    end
 end
