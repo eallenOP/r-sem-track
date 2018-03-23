8e4ed5672ce941c74969b64fc9fc4aefc7ce809c
Elise <elise.allen@op.ac.nz>
2018-03-23 16:46:04 +1300

    Add semester index

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,4 +1,8 @@
 class SemestersController < ApplicationController
+    def index
+        @semesters = Semester.all
+    end
+    
     def show
         @semester = Semester.find(params[:id])
     end
